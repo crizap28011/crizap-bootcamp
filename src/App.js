e04d8d73a8c3915e2345630ed31d14bf619f326e
Cristian Zapata <cristian.zapata28011@gmail.com>
2021-12-20 00:13:00 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Mensaje from './components/Mensaje'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mensaje mensaje='holaaaaaaaaaaa'/>
+      <Mensaje mensaje='perroooooooooooooo'/>
+      <Mensaje mensaje='que se cuentaaaaaaa'/>
     </div>
   );
 }
---
+++ src/components/Mensaje.js
@@ -0,0 +1,6 @@
+const Mensaje = ({mensaje}) => {
+
+    return <h1>{mensaje}</h1>
+}
+
+export default Mensaje
